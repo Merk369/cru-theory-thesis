2b1330efe5cc15a8743a52132612e390c2aff2f0
Merk369 <shaun.cleary@email.phoenix.edu>
2025-09-05 14:41:06 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,22 @@
+# Configuration file for the Sphinx documentation builder.
 
+project = 'Causal Resonance Unification (CRU) Thesis'
+copyright = '2025, The Cleary Group LLC'
+author = 'Shaun Cleary'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.githubpages',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
